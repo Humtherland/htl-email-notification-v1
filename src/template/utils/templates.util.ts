7b05a7ebe3953a099b2784e81247fab1b7a75b30
samuel14luis <luismendoza.samuel@gmail.com>
2023-05-06 01:16:38 -0500

    feat: generate random name

--- src/template/utils/templates.util.ts
+++ src/template/utils/templates.util.ts
@@ -1,14 +1,14 @@
+import { v4 as uuid } from 'uuid';
+
 export const renameFile = (req, file, callback) => {
 
-    const name = file.originalname.split('.')[0];
     const fileExtName = file.originalname.split('.')[1];
     const randomName = Array(4)
         .fill(null)
         .map(() => Math.round(Math.random() * 16).toString(16))
         .join('');
 
-    console.log(`${name}-${randomName}.${fileExtName}`);
-    callback(null, `${name}-${randomName}-${Date.now()}.${fileExtName}`);
+    callback(null, `${randomName}-${uuid()}.${fileExtName}`);
   }
 
 export const destinationFile = (req, file, callback) => {

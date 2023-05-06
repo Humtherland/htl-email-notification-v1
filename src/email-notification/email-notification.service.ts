6770e2b64118bbb6b0f65590f8d9403f97a0ff7f
samuel14luis <luismendoza.samuel@gmail.com>
2023-05-05 20:45:53 -0500

    feat: add email notification logic

--- src/email-notification/email-notification.service.ts
+++ src/email-notification/email-notification.service.ts
@@ -5,5 +5,31 @@ import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class EmailNotificationService {
-  async sendEmail(dto: SendEmailNotificationDto) { return "TO IMPLEMENT"; }
+  async sendEmail(dto: SendEmailNotificationDto) {
+    try {
+      const config = {
+        host: EmailNotificationModule.host,
+        port: EmailNotificationModule.port,
+        auth: {
+          user: EmailNotificationModule.user,
+          pass: EmailNotificationModule.password
+        }
+      }
+      const mensaje = {
+        to: dto.to,
+        subject: dto.subject,
+        html: dto.text
+      }
+
+      let transporter:any = nodemailer.createTransport(config);
+      let info = await transporter.sendMail(mensaje);
+      return info;
+    } catch (error) {
+      this.errorHandler(error);
+    }
+  }
+
+  private errorHandler( error: any ): any {
+    if (error.errno == '-4078') throw new InternalServerErrorException('Error de conexión con el servidor de correo.');
+  }
 }
